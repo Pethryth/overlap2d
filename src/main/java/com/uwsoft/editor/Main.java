cfabf74ba73b3a102de5b9a65912f631f59c546f
Simone Arpe <simon.arpe@gmail.com>
2019-08-05 20:28:33 +0200

    Migrate from LwjglFrame to LwjglApplication
    Signed-off-by: Simone Arpe <simon.arpe@gmail.com>

--- src/main/java/com/uwsoft/editor/Main.java
+++ src/main/java/com/uwsoft/editor/Main.java
@@ -18,38 +18,24 @@
 
 package com.uwsoft.editor;
 
-import java.awt.GraphicsEnvironment;
-import java.awt.Image;
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.dnd.DropTarget;
-import java.awt.event.InputEvent;
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.URL;
-
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-
-import com.uwsoft.editor.view.frame.FileDropListener;
-import org.apache.commons.lang3.SystemUtils;
-
-import com.badlogic.gdx.backends.jglfw.JglfwApplication;
-import com.badlogic.gdx.backends.jglfw.JglfwApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.backends.lwjgl.LwjglFrame;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.uwsoft.editor.splash.SplashStarter;
 import com.uwsoft.editor.utils.AppConfig;
 
+import org.apache.commons.lang3.SystemUtils;
+
+import javax.swing.UIManager;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.io.File;
+import java.net.URL;
+
 public class Main {
 
     private SplashStarter splash;
-    private LwjglFrame mainFrame;
+    private LwjglApplication mainFrame;
 
     public Main() {
         splash = new SplashStarter(() -> startLoadingEditor());
@@ -73,22 +59,25 @@ public class Main {
         config.width = (int) (width);
         config.height = (int) (height - height * .04);
         config.backgroundFPS = 60;
-        mainFrame = new LwjglFrame(overlap2D, config);
+        mainFrame = new LwjglApplication(overlap2D, config);
         if (SystemUtils.IS_OS_MAC_OSX || SystemUtils.IS_OS_MAC) {
             System.setProperty("apple.laf.useScreenMenuBar", "true");
             System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Overlap2D");
-        } else {
-            mainFrame.setExtendedState(mainFrame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         }
-        toggleVisible();
-
-        // subscribe to file dropping notifications, currently windows only
-        DropTarget dropTarget = new DropTarget(mainFrame, new FileDropListener());
-
-        if (!SystemUtils.IS_OS_UNIX) {
-            // no aesthetics for linux users I guess..
-            setIcon();
-        }
+//        else {
+//            mainFrame.setExtendedState(mainFrame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+//        }
+//        mainFrame.setMinimumSize(new Dimension(500, 600));
+//        mainFrame.setResizable(true);
+//        toggleVisible();
+//
+//        // subscribe to file dropping notifications, currently windows only
+//        DropTarget dropTarget = new DropTarget(mainFrame, new FileDropListener());
+//
+//        if (!SystemUtils.IS_OS_UNIX) {
+//            // no aesthetics for linux users I guess..
+//            setIcon();
+//        }
 
     }
 
@@ -143,64 +132,64 @@ public class Main {
     }
 
 
-    private void toggleVisible() {
-        mainFrame.setVisible(!mainFrame.isVisible());
-        if (mainFrame.isVisible()) {
-            mainFrame.toFront();
-            mainFrame.requestFocus();
-            mainFrame.setAlwaysOnTop(true);
-            try {
-                //remember the last location of mouse
-                final Point oldMouseLocation = MouseInfo.getPointerInfo().getLocation();
-
-                //simulate a mouse click on title bar of window
-                Robot robot = new Robot();
-                robot.mouseMove(mainFrame.getX() + 100, mainFrame.getY() + 5);
-                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
-                //move mouse to old location
-                robot.mouseMove((int) oldMouseLocation.getX(), (int) oldMouseLocation.getY());
-            } catch (Exception ex) {
-                //just ignore exception, or you can handle it as you want
-            } finally {
-                mainFrame.setAlwaysOnTop(false);
-            }
-        }
-    }
+//    private void toggleVisible() {
+//        mainFrame.setVisible(!mainFrame.isVisible());
+//        if (mainFrame.isVisible()) {
+//            mainFrame.toFront();
+//            mainFrame.requestFocus();
+//            mainFrame.setAlwaysOnTop(true);
+//            try {
+//                //remember the last location of mouse
+//                final Point oldMouseLocation = MouseInfo.getPointerInfo().getLocation();
+//
+//                //simulate a mouse click on title bar of window
+//                Robot robot = new Robot();
+//                robot.mouseMove(mainFrame.getX() + 100, mainFrame.getY() + 5);
+//                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+//
+//                //move mouse to old location
+//                robot.mouseMove((int) oldMouseLocation.getX(), (int) oldMouseLocation.getY());
+//            } catch (Exception ex) {
+//                //just ignore exception, or you can handle it as you want
+//            } finally {
+//                mainFrame.setAlwaysOnTop(false);
+//            }
+//        }
+//    }
 
     //THIS IS JUST FOR FUN
-    private void setIcon(){
-    	String logoPath = getLocalArtPath("splash_textures");
-        File file = new File(logoPath);
-        if(!file.exists()){
-        	 logoPath = "art/splash_textures/";
-             file = new File(logoPath);
-        }
-        logoPath+="icon.png";
-
-    	if(mainFrame != null){
-    		mainFrame.setIconImage(Toolkit.getDefaultToolkit().getImage(logoPath));
-    		return;
-    	}
-
-    	 try {
-             Class util = Class.forName("com.apple.eawt.Application");
-             Method getApplication = util.getMethod("getApplication", new Class[0]);
-             Object application = getApplication.invoke(util);
-             Class params[] = new Class[1];
-             params[0] = Image.class;
-             Method setDockIconImage = util.getMethod("setDockIconImage", params);
-             Image image = Toolkit.getDefaultToolkit().getImage(logoPath);
-             setDockIconImage.invoke(application, image);
-         } catch (ClassNotFoundException e) {
-             // log exception
-         } catch (NoSuchMethodException e) {
-             // log exception
-         } catch (InvocationTargetException e) {
-             // log exception
-         } catch (IllegalAccessException e) {
-             // log exception
-         }
-    }
+//    private void setIcon(){
+//    	String logoPath = getLocalArtPath("splash_textures");
+//        File file = new File(logoPath);
+//        if(!file.exists()){
+//        	 logoPath = "art/splash_textures/";
+//             file = new File(logoPath);
+//        }
+//        logoPath+="icon.png";
+//
+//    	if(mainFrame != null){
+//    		mainFrame.setIconImage(Toolkit.getDefaultToolkit().getImage(logoPath));
+//    		return;
+//    	}
+//
+//    	 try {
+//             Class util = Class.forName("com.apple.eawt.Application");
+//             Method getApplication = util.getMethod("getApplication", new Class[0]);
+//             Object application = getApplication.invoke(util);
+//             Class params[] = new Class[1];
+//             params[0] = Image.class;
+//             Method setDockIconImage = util.getMethod("setDockIconImage", params);
+//             Image image = Toolkit.getDefaultToolkit().getImage(logoPath);
+//             setDockIconImage.invoke(application, image);
+//         } catch (ClassNotFoundException e) {
+//             // log exception
+//         } catch (NoSuchMethodException e) {
+//             // log exception
+//         } catch (InvocationTargetException e) {
+//             // log exception
+//         } catch (IllegalAccessException e) {
+//             // log exception
+//         }
+//    }
 }
